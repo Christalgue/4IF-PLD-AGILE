7b85b05c1441798b81aa9b608964492d40b94964
WingOfRaven <lea.ferrere@insa-lyon.fr>
2018-11-26 12:05:52 +0100

    Creation of state class + controller

--- src/main/java/controller/InitialState.java
+++ src/main/java/controller/InitialState.java
@@ -1,5 +1,14 @@
 package main.java.controller;
 
+import main.java.view.Window;
+
 public class InitialState extends DefaultState {
+	
+	public void loadMap(Controller controller, Window window, String filename) {
+		
+		
+		controller.setCurrentState(controller.mapLoadedState);
+	}
+	
 
 }
--- src/main/java/controller/State.java
+++ src/main/java/controller/State.java
@@ -4,21 +4,30 @@ import main.java.view.Window;
 
 public interface State {
 	
-	public void loadMap();
+	public void loadMap(Controller controler, Window window, String filename);
 	
-	public void loadDeliveryOffer();
+	public void loadDeliveryOffer(Controller controler, Window window, String filename);
 	
-	public void calculateCircuits();
+	public void calculateCircuits(Controller controler, Window window);
 	
-	public void undo();
+	//public void undo();
 	
-	public void addDelivery();
+	public void addDelivery(Controller controler, Window window);
 	
-	public void validate();
+	public void validate(Controller controler, Window window);
 	
-	public void cancel();
+	public void cancel(Controller controler, Window window);
 	
-	public void moveDelivery ();
+	public void moveDelivery (Controller controler, Window window);
 	
-	public void deleteDelivery();
+	public void deleteDelivery(Controller controler, Window window);
+	
+	
+	public void rightClick(Controller controler, Window window);
+	
+	public void leftClick(Controller controler, Window window);
+	
+	public void movedMouse(Controller controler, Window window);
+	
+	
 }
