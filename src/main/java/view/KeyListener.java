d67804092e6372bdd94e17dbdc3a5a2207b4ed66
a8bdx4rm <mathilde.goigoux@orange.fr>
2018-11-26 09:46:21 +0100

    Key listener added

---
+++ src/main/java/view/KeyListener.java
@@ -0,0 +1,23 @@
+package main.java.view;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import main.java.controller.Controller;
+
+public class KeyListener extends KeyAdapter {
+
+	private Controller controller;
+
+	public KeyListener(Controller controller){
+		this.controller = controller;
+	}
+	
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// Called by the key listener each time a key is pressed
+		// Interesting keys are SUPPR, CRTL+Z, and numbers ( for delivery men number ) 
+		controller.keyPressed(e.getKeyCode());
+	}
+
+}
