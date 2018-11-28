685ae638fb44484e2b57e835e8b7073963cab83c
Martin <grecomartin@icloud.com>
2018-11-28 11:36:40 +0100

    added TestLoadMap and TestLoadDeliveries to TestCircuitManagement

--- src/test/java/TestCircuitManagement.java
+++ src/test/java/TestCircuitManagement.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.TimeZone;
 
 import javax.xml.parsers.ParserConfigurationException;
 
@@ -12,6 +13,7 @@ import org.junit.jupiter.api.Test;
 import org.xml.sax.SAXException;
 
 import javafx.util.Pair;
+import main.java.entity.Bow;
 import main.java.entity.CircuitManagement;
 import main.java.entity.Delivery;
 import main.java.entity.Map;
@@ -24,19 +26,66 @@ import main.java.utils.Deserializer;
 class TestCircuitManagement {
 
 	@Test
+	void testLoadMap() {
+		try {
+			CircuitManagement circuitManager = new CircuitManagement();
+			circuitManager.loadMap("resources/tests/Global/xml/plan_simple.xml");
+			
+			Map map = circuitManager.getCurrentMap();
+			
+			assertTrue(map.getNodeMap().containsKey((long)1),"Nodes do not contain key 1");
+			assertTrue(map.getNodeMap().containsKey((long)2),"Nodes do not contain key 2");
+			assertTrue(map.getNodeMap().get((long)1).getId()==1,"wrong ID (1) : "+map.getNodeMap().get((long)1).getId());
+			assertTrue(map.getNodeMap().get((long)1).getLatitude()==45.75406,"wrong latitude (45.75406) : "+map.getNodeMap().get((long)1).getLatitude());
+			assertTrue(map.getNodeMap().get((long)1).getLongitude()==4.857418,"wrong longitude (4.857418) : "+map.getNodeMap().get((long)1).getLongitude());
+			assertTrue(map.getNodeMap().get((long)2).getId()==2,"wrong ID (2) : "+map.getNodeMap().get((long)2).getId());
+			assertTrue(map.getNodeMap().get((long)2).getLatitude()==45.750404,"wrong latitude (45.750404) : "+map.getNodeMap().get((long)2).getLatitude());
+			assertTrue(map.getNodeMap().get((long)2).getLongitude()==4.8744674,"wrong longitude (4.8744674) : "+map.getNodeMap().get((long)2).getLongitude());
+			assertTrue(map.getBowMap().containsKey((long)1),"Bows do not contain key");
+			Bow first = map.getBowMap().get((long)1).iterator().next();
+			assertTrue(first.getStartNode().getId()==1,"wrong start node (1) : "+first.getStartNode().getId());
+			assertTrue(first.getEndNode().getId()==2,"wrong end node (2) : "+first.getEndNode().getId());
+			assertTrue(first.getLength()==79.801414,"wrong length (79.801414) : "+first.getLength());
+			assertTrue(first.getStreetName().contains("Rue Edouard Aynard"),"wrong street name (Rue Edouard Aynard) : "+first.getStreetName());
+		} catch (LoadMapException e) {
+			fail("LoadMapException, report to TestDeserializer : "+e.getMessage());
+		}
+	}
+	
+	@Test
+	void testLoadDeliveries() {
+		try {
+			CircuitManagement circuitManager = new CircuitManagement();
+			circuitManager.loadMap("resources/tests/Global/xml/plan_simple.xml");
+			circuitManager.loadDeliveryList("resources/tests/Global/xml/delivery_simple.xml");
+			List<Delivery> deliveries = circuitManager.getDeliveryList();
+			
+			int UTC = TimeZone.getDefault().getRawOffset();
+			assertTrue(deliveries.get(0).getClass().getName().contains("main.java.entity.Repository"),"8) First delivery is not repository");
+			assertTrue(deliveries.get(0).getDuration()==0,"8) Wrong duration (0)"+deliveries.get(0).getDuration());
+			assertTrue(deliveries.get(0).getHourOfArrival().getTimeInMillis()+UTC==28800000,"8) Wrong hour of departure (28800000)"+(deliveries.get(0).getHourOfArrival().getTimeInMillis()+UTC));
+			assertTrue(deliveries.get(0).getHourOfDeparture().getTimeInMillis()+UTC==28800000,"8) Wrong hour of arrival (28800000)"+(deliveries.get(0).getHourOfDeparture().getTimeInMillis()+UTC));
+			assertTrue(deliveries.get(0).getPosition().getId()==1,"8) Wrong node (1)"+deliveries.get(0).getPosition());
+			assertTrue(deliveries.get(1).getDuration()==60,"8) Wrong duration (60)"+deliveries.get(1).getDuration());
+			assertTrue(deliveries.get(1).getPosition().getId()==2,"8) Wrong node (2)"+deliveries.get(1).getPosition());
+		} catch (LoadMapException e) {
+			fail("LoadMapException, report to TestDeserializer : "+e.getMessage());
+		} catch (LoadDeliveryException e) {
+			fail("LoadDeliveryException, report to TestDeserializer : "+e.getMessage());
+		}
+	}
+	
+	@Test
 	void testCluster() {
 		
 		try {
-			/*Map map = new Map("resources/tests/Cluster/xml/plan.xml");
-			List<Delivery> arrivalDeliveries = Deserializer.loadDeliveries("resources/tests/Cluster/xml/delivery.xml", map);*/
+			CircuitManagement circuitManager = new CircuitManagement();
+			circuitManager.loadMap("resources/tests/Global/xml/plan.xml");
+			circuitManager.loadDeliveryList("resources/tests/Global/xml/delivery.xml");
 			
-			CircuitManagement CircuitManager = new CircuitManagement();
-			CircuitManager.loadMap("resources/tests/Global/xml/plan.xml");
-			CircuitManager.loadDeliveryList("resources/tests/Global/xml/delivery.xml");
-			
-			CircuitManager.setNbDeliveryMan(2);
+			circuitManager.setNbDeliveryMan(2);
 	    	
-	    	List<ArrayList<Delivery>> distribution = CircuitManager.cluster();
+	    	List<ArrayList<Delivery>> distribution = circuitManager.cluster();
 	    	int nbDeliveries1 = distribution.get(0).size();
 	    	int nbDeliveries2 = distribution.get(1).size();
 	    	
@@ -52,20 +101,27 @@ class TestCircuitManagement {
 
 		} catch (LoadMapException e) {
 			fail("LoadMapException, report to TestDeserializer : "+e.getMessage());
-			/*
-		} catch (ParserConfigurationException e) {
-			fail("ParserConfigurationException, report to TestDeserializer");
-		} catch (SAXException e) {
-			fail("SAXException, report to TestDeserializer");
-		} catch (IOException e) {
-			fail("IOException, report to TestDeserializer");
-		} catch (XMLException e) {
-			fail("XMLException, report to TestDeserializer : "+e.getMessage()); */
 		} catch (ClusteringException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			fail("ClusteringException"+e.getMessage());
 		} catch (LoadDeliveryException e) {
-			e.printStackTrace();
+			fail("LoadDeliveryException"+e.getMessage());
+		}
+	}
+	
+	void testAddDelivery() {
+		try {
+			CircuitManagement circuitManager = new CircuitManagement();
+			circuitManager.loadMap("resources/tests/Global/xml/plan_simple.xml");
+			circuitManager.loadDeliveryList("resources/tests/Global/xml/delivery_simple.xml");
+			
+			//TODO
+
+		} catch (LoadMapException e) {
+			fail("LoadMapException, report to TestDeserializer : "+e.getMessage());
+		} catch (ClusteringException e) {
+			fail("ClusteringException"+e.getMessage());
+		} catch (LoadDeliveryException e) {
+			fail("LoadDeliveryException"+e.getMessage());
 		}
 	}
 
