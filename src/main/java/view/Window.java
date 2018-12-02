dd637edef59903fa1c1b279669e34b6dca4ec510
Miphilde <mathilde.goigoux@orange.fr>
2018-12-02 12:09:45 +0100

    TextualView and messageField added

--- src/main/java/view/Window.java
+++ src/main/java/view/Window.java
@@ -5,9 +5,11 @@ import java.awt.TextField;
 import java.io.File;
 import java.io.IOException;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
@@ -21,12 +23,23 @@ public class Window extends JFrame{
 	private Controller controller;
 	private CircuitManagement circuitManagement;
 	private GraphicView graphicView;
+	private TextualView textualView;
+	
+	protected JLabel messageField;
+	
+	private static JFileChooser chooser;
+
+	protected static ButtonsListener buttonsListener;
+	protected static MouseListener mouseListener;	
+	
 	protected static final String LOAD_MAP = "Charger un plan";
 	protected static final String LOAD_DELIVERY_OFFER = "Charger une demande de livraison";
 	protected static final String CALCULATE_CIRCUITS = "Calculer les tournees";
 	protected static final String ADD_DELIVERY = "Ajouter une livraison";
-	protected static final String CONTINUE_CALCULATION = "Continuer le calcul des chemins";
-	protected static final String STOP_CALCULATION = "Arrêter le calcul des chemins";
+	protected static final String DELETE_DELIVERY = "Supprimer la livraison";
+	protected static final String MOVE_DELIVERY = "Deplacer la livraison";
+	protected static final String CONTINUE_CALCULATION = "Continuer le calcul des tournees";
+	protected static final String STOP_CALCULATION = "Arrêter le calcul des tournees";
 	
 	protected static TextField setNameOfMap;
 	protected static TextField setNameOfDeliveryList;
@@ -35,16 +48,14 @@ public class Window extends JFrame{
 	protected static JButton loadDeliveryList;
 	protected static JButton calculateCircuitButton;
 	protected static JButton addDeliveryButton;
-	
-	private static JFileChooser chooser;
-	
-	protected static ButtonsListener buttonsListener;
-	protected static MouseListener mouseListener;
+	protected static JButton deleteDeliveryButton;
+	protected static JButton moveDeliveryButton;
 	
 	protected static final int windowWidth = 1280;
 	protected static final int windowHeight = 720;
 	protected static final int buttonPanelHeight =50;
 	protected static final int graphicWidth = 1080;
+	protected static final int messageFieldHeight = 40;
 	
 	protected static final int pathWidth = 3;
 	
@@ -73,7 +84,7 @@ public class Window extends JFrame{
 		Controller controller = new Controller(new CircuitManagement());
 		buttonsListener = new ButtonsListener(controller);
 		
-		//////////////////////////////CREATION OF THE MAIN WINDOW//////////////////////////////
+		//////////////////////////////CREATE THE MAIN WINDOW//////////////////////////////
 		
 		setControllerWindow(controller.getWindow());	
 		
@@ -88,13 +99,20 @@ public class Window extends JFrame{
 		mouseListener = new MouseListener(controller, controller.getWindow().graphicView,controller.getWindow());
 		controller.getWindow().addMouseListener(mouseListener);
 		
-		//////////////////////////////CREATE THE TEXTUAL PANEL/////////////////////////////
-		JPanel textualPanel = new JPanel();
-		fillTextualPanel(textualPanel);
+		//////////////////////////////CREATE THE TEXTUAL VIEW/////////////////////////////
+		
+		controller.getWindow().textualView = new TextualView (controller.getWindow().circuitManagement, windowHeight-buttonPanelHeight, windowWidth-graphicWidth);
+		setTextualView(controller.getWindow().textualView);
+		
+		//////////////////////////////CREATE THE MESSAGE FIELD/////////////////////////////
+		
+		controller.getWindow().messageField = new JLabel();
+		setMessageField(controller.getWindow().messageField);
 		
 		//////////////////////////////ADD PANELS TO THE WINDOW//////////////////////////////
 		controller.getWindow().getContentPane().add(buttonPanel);
-		controller.getWindow().getContentPane().add(textualPanel);
+		controller.getWindow().getContentPane().add(controller.getWindow().textualView);
+		controller.getWindow().getContentPane().add(controller.getWindow().messageField);
 		controller.getWindow().getContentPane().add(controller.getWindow().graphicView);
 		
 		////////////////////////////// MAKE THE FRAME VISBLE AND PAINTABLE ////////////////////////////////
@@ -102,8 +120,114 @@ public class Window extends JFrame{
 		
 		controller.getWindow().graphicView.setGraphics();
 		
+	}	
+	
+	private static void setMessageField(JLabel messageField) {
+		messageField.setSize( graphicWidth, messageFieldHeight);
+		messageField.setLocation(0, buttonPanelHeight);
+		
+	}
+
+	public static void setControllerWindow( Window window) {
+		
+		window.setLayout(null);
+		window.setSize(windowWidth, windowHeight);
+		window.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		window.setTitle("Deliver'IF");
+		window.setResizable(false);
+		window.setLocationRelativeTo(null);
+	
 	}
 	
+	private static void setGraphicView(GraphicView graphicView) {
+		
+		graphicView.setLocation(0, buttonPanelHeight);
+		graphicView.setLayout(null);
+		graphicView.setBackground(Color.LIGHT_GRAY);
+		graphicView.setSize(graphicWidth, windowHeight-buttonPanelHeight);
+		
+	}
+	
+	private static void setTextualView(TextualView textualView) {
+		
+		textualView.setLocation(graphicWidth, buttonPanelHeight);
+		textualView.setLayout(null);
+		textualView.setBackground(Color.WHITE);
+		textualView.setSize(windowWidth-graphicWidth, windowHeight-buttonPanelHeight);
+		
+		textualView.setBorder(BorderFactory.createTitledBorder("Vue Textuelle"));
+		
+		addDeliveryButton = new JButton(ADD_DELIVERY);
+		addDeliveryButton.addActionListener(buttonsListener);
+		addDeliveryButton.setVisible(false);
+		textualView.add(addDeliveryButton);
+	
+		deleteDeliveryButton = new JButton(DELETE_DELIVERY);
+		deleteDeliveryButton.addActionListener(buttonsListener);
+		deleteDeliveryButton.setVisible(false);
+		textualView.add(deleteDeliveryButton);	
+	
+		moveDeliveryButton = new JButton(MOVE_DELIVERY);
+		moveDeliveryButton.addActionListener(buttonsListener);
+		moveDeliveryButton.setVisible(false);
+		textualView.add(moveDeliveryButton);	
+	
+	}
+	
+	public static void fillButtonPanel(JPanel buttonPanel) {
+		
+		buttonPanel.setSize(windowWidth, buttonPanelHeight);
+		buttonPanel.setBackground(Color.WHITE);
+		
+		setNameOfMap = new TextField();
+		setNameOfMap.setText("resources/xml/grandPlan.xml");
+		buttonPanel.add(setNameOfMap);
+		
+		JButton loadMapButton = new JButton(LOAD_MAP);
+		loadMapButton.addActionListener(buttonsListener);
+		buttonPanel.add(loadMapButton);
+		
+		setNameOfDeliveryList = new TextField();
+		setNameOfDeliveryList.setText("resources/xml/dl-grand-20.xml");
+		buttonPanel.add(setNameOfDeliveryList);
+		
+		loadDeliveryList = new JButton(LOAD_DELIVERY_OFFER);
+		loadDeliveryList.addActionListener(buttonsListener);
+		loadDeliveryList.setEnabled(false);
+		buttonPanel.add(loadDeliveryList);
+		
+		JTextArea labelNumberOfDeliveryMen = new JTextArea();
+		labelNumberOfDeliveryMen.setText("Nombre de livreurs :");
+		labelNumberOfDeliveryMen.setEditable(false);
+		buttonPanel.add(labelNumberOfDeliveryMen);
+		
+		numberOfDeliveryMen = new TextField();
+		numberOfDeliveryMen.setText("1");
+		buttonPanel.add(numberOfDeliveryMen);
+	
+		/*JButton undoButton = new JButton("Retour");
+		buttonPanel.add(undoButton);
+		undoButton.addActionListener(buttonsListener);*/
+		
+		calculateCircuitButton = new JButton(CALCULATE_CIRCUITS);
+		calculateCircuitButton.addActionListener(buttonsListener);
+		calculateCircuitButton.setEnabled(false);
+		buttonPanel.add(calculateCircuitButton);
+	}
+	
+	
+	protected String getFile () {
+			
+		chooser = new JFileChooser();
+		int returnValue = chooser.showOpenDialog(controller.getWindow());
+		String filePath = "";
+		
+		if(returnValue == JFileChooser.APPROVE_OPTION){
+			filePath = chooser.getSelectedFile().getAbsolutePath();
+		}
+		return filePath;
+	}
+
 	public static void getDeliveryMenNumber() {
 		
 		buttonsListener.setDeliveryMenNumber(Integer.parseInt(numberOfDeliveryMen.getText()));
@@ -120,6 +244,16 @@ public class Window extends JFrame{
 		
 		buttonsListener.setDeliveryList(setNameOfDeliveryList.getText());
 		
+	}
+	
+	public void setTextualViewBorderTitle( String string) {	
+		textualView.setBorder(BorderFactory.createTitledBorder(string));
+	}
+	
+	public void setMessage( String string, Color color) {
+		messageField.setText(string);
+		//messageField.setBackground(color);
+		messageField.setForeground(color);
 	}
 	
 	public void drawMap() {
@@ -142,97 +276,4 @@ public class Window extends JFrame{
 		graphicView.paintSelectedNode(graphicView.getGraphic(), node, selectedColor);
 	}
 	
-	public static void setControllerWindow( Window window) {
-		
-		window.setLayout(null);
-		window.setSize(windowWidth, windowHeight);
-		window.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		window.setTitle("Deliver'IF");
-		window.setResizable(false);
-		window.setLocationRelativeTo(null);
-	
-	}
-	
-	private static void setGraphicView(GraphicView graphicView) {
-		
-		graphicView.setLocation(0, buttonPanelHeight);
-		graphicView.setLayout(null);
-		graphicView.setBackground(Color.LIGHT_GRAY);
-		graphicView.setSize(graphicWidth, windowHeight-buttonPanelHeight);
-		
-	}
-	
-	public static void fillButtonPanel(JPanel buttonPanel) {
-		
-		buttonPanel.setSize(windowWidth, buttonPanelHeight);
-		buttonPanel.setBackground(Color.WHITE);
-		
-		setNameOfMap = new TextField();
-		setNameOfMap.setText("resources/xml/grandPlan.xml");
-		buttonPanel.add(setNameOfMap);
-		
-		JButton loadMapButton = new JButton(LOAD_MAP);
-		loadMapButton.addActionListener(buttonsListener);
-		buttonPanel.add(loadMapButton);
-		
-		/*JButton loadMapButton = new JButton(LOAD_MAP);
-		loadMapButton.addActionListener(buttonsListener);
-		buttonPanel.add(loadMapButton);*/
-		
-		setNameOfDeliveryList = new TextField();
-		setNameOfDeliveryList.setText("resources/xml/dl-grand-20.xml");
-		buttonPanel.add(setNameOfDeliveryList);
-		
-		loadDeliveryList = new JButton(LOAD_DELIVERY_OFFER);
-		loadDeliveryList.addActionListener(buttonsListener);
-		loadDeliveryList.setEnabled(false);
-		buttonPanel.add(loadDeliveryList);
-		
-		/*loadDeliveryList = new JButton(LOAD_DELIVERY_OFFER);
-		loadDeliveryList.addActionListener(buttonsListener);
-		loadDeliveryList.setEnabled(false);
-		buttonPanel.add(loadDeliveryList);*/
-		
-		JTextArea labelNumberOfDeliveryMen = new JTextArea();
-		labelNumberOfDeliveryMen.setText("Nombre de livreurs :");
-		labelNumberOfDeliveryMen.setEditable(false);
-		buttonPanel.add(labelNumberOfDeliveryMen);
-		
-		numberOfDeliveryMen = new TextField();
-		numberOfDeliveryMen.setText("1");
-		buttonPanel.add(numberOfDeliveryMen);
-	
-		/*JButton undoButton = new JButton("Retour");
-		buttonPanel.add(undoButton);
-		undoButton.addActionListener(buttonsListener);*/
-		
-		calculateCircuitButton = new JButton(CALCULATE_CIRCUITS);
-		calculateCircuitButton.addActionListener(buttonsListener);
-		calculateCircuitButton.setEnabled(false);
-		buttonPanel.add(calculateCircuitButton);
-	}
-	
-	private static void fillTextualPanel(JPanel textualPanel) {
-		textualPanel.setSize(windowWidth-graphicWidth, windowHeight-buttonPanelHeight);
-		textualPanel.setLocation(graphicWidth, buttonPanelHeight);
-		textualPanel.setBackground(Color.GREEN);
-		
-		addDeliveryButton = new JButton(ADD_DELIVERY);
-		addDeliveryButton.addActionListener(buttonsListener);
-		addDeliveryButton.setVisible(false);
-		textualPanel.add(addDeliveryButton);
-	}
-	
-	protected String getFile () {
-			
-		chooser = new JFileChooser();
-		int returnValue = chooser.showOpenDialog(controller.getWindow());
-		String filePath = "";
-		
-		if(returnValue == JFileChooser.APPROVE_OPTION){
-			filePath = chooser.getSelectedFile().getAbsolutePath();
-		}
-		return filePath;
-	}
-
 }
